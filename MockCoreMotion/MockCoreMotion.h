38fbbb1351e1a47169104900b4c37f9f35f3f5b6
Chunyu Ou <Chunyu@UnifyIDs-MacBook-Pro-2.local>
2017-03-11 16:19:58 -0800

    Initial Commit

---
+++ MockCoreMotion/MockCoreMotion.h
@@ -0,0 +1,19 @@
+//
+//  MockCoreMotion.h
+//  MockCoreMotion
+//
+//  Created by Chunyu Ou on 3/11/17.
+//  Copyright © 2017 Chunyu Ou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MockCoreMotion.
+FOUNDATION_EXPORT double MockCoreMotionVersionNumber;
+
+//! Project version string for MockCoreMotion.
+FOUNDATION_EXPORT const unsigned char MockCoreMotionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockCoreMotion/PublicHeader.h>
+
+

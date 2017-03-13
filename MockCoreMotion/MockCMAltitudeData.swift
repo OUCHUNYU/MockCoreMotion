2ab93e7f9fbe4cced8d7a902bf4d20633407d771
Chunyu Ou <Chunyu@UnifyIDs-MacBook-Pro-2.local>
2017-03-12 22:29:41 -0700

    add MockCMAltitudeData and tests

--- MockCoreMotion/MockCMAltitudeData.swift
+++ MockCoreMotion/MockCMAltitudeData.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import CoreMotion
+
+class MockCMAltitudeData: CMAltitudeData {
+    
+    // The change in altitude (in meters) since the last reported event.
+    // We set default to 0 meannig we are at 0 meter
+    private var _relativeAltitude: NSNumber = 0
+    
+    // As Altitude set to 0 and assume air temperture around 10 - 20 degree C
+    // the air pressure set to default 101325.00
+    private var _pressure: NSNumber = 101325.00
+    
+    private var _timestamp: TimeInterval = Date().timeIntervalSinceReferenceDate
+    
+    override var relativeAltitude: NSNumber {
+        get {
+            return _relativeAltitude
+        }
+        set {
+            _relativeAltitude = newValue
+        }
+    }
+    
+    override var pressure: NSNumber {
+        get {
+            return _pressure
+        }
+        set {
+            _pressure = newValue
+        }
+    }
+    
+    override open var timestamp: TimeInterval {
+        get {
+            return _timestamp
+        }
+        set {
+            _timestamp = newValue
+        }
+    }
+
+}
---
+++ MockCoreMotionTests/MockCMAltitudeDataTests.swift
@@ -0,0 +1,41 @@
+//
+//  MockCMAltitudeDataTests.swift
+//  MockCoreMotion
+//
+//  Created by Chunyu Ou on 3/12/17.
+//  Copyright © 2017 Chunyu Ou. All rights reserved.
+//
+
+import XCTest
+@testable import MockCoreMotion
+
+class MockCMAltitudeDataTests: XCTestCase {
+    
+    var testMockCMAltitudeData: MockCMAltitudeData?
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testMockCMAltitudeData = MockCMAltitudeData()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        testMockCMAltitudeData = nil
+        super.tearDown()
+    }
+    
+    func testRelativeAltitude() {
+        XCTAssertEqual(testMockCMAltitudeData!.relativeAltitude, 0, "A new MockCMAltitudeData instance has relativeAltitude 0 meter")
+        testMockCMAltitudeData?.relativeAltitude = 100
+        XCTAssertEqual(testMockCMAltitudeData!.relativeAltitude, 100)
+    }
+    
+    func testPressure() {
+        XCTAssertEqual(testMockCMAltitudeData!.pressure, 101325.00, "A new MockCMAltitudeData instance has pressure 101325.00 based on 0 meter altitue and 10 - 20 degree C air temperture.")
+        let testPressure = 131325.00
+        testMockCMAltitudeData?.pressure = testPressure as NSNumber
+        XCTAssertEqual(testMockCMAltitudeData?.pressure, testPressure as NSNumber)
+    }
+    
+}

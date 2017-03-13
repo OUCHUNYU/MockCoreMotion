5a1f8289592a7ab47e8bf95de59e7b73c50610c3
Chunyu Ou <Chunyu@UnifyIDs-MacBook-Pro-2.local>
2017-03-13 10:24:47 -0700

    add mock attitude and tests

--- MockCoreMotion/MockCMAttitude.swift
+++ MockCoreMotion/MockCMAttitude.swift
@@ -7,3 +7,71 @@
 //
 
 import Foundation
+import CoreMotion
+
+class MockCMAttitude: CMAttitude {
+    private var _roll: Double = 0
+    private var _yaw: Double = 0
+    private var _pitch: Double = 0
+    // CMRotationMatrix has initalizer that takes data such as m11 m12 m13 etc,
+    // for this one it is set to all 0 as other properties
+    private var _rotationMatrix: CMRotationMatrix = CMRotationMatrix()
+    // CMQuaternion has initalizer that takes data such as x y z w as Double,
+    // for this one it is set to all 0 as other properties
+    private var _quaternion: CMQuaternion = CMQuaternion()
+    
+    open override var roll: Double {
+        get {
+            return _roll
+        }
+        set {
+            _roll = newValue
+        }
+    }
+    
+    open override var yaw: Double {
+        get {
+            return _yaw
+        }
+        set {
+            _yaw = newValue
+        }
+    }
+    
+    open override var pitch: Double {
+        get {
+            return _pitch
+        }
+        set {
+            _pitch = newValue
+        }
+    }
+
+    open override var rotationMatrix: CMRotationMatrix {
+        get {
+            return _rotationMatrix
+        }
+        set {
+            _rotationMatrix = newValue
+        }
+    }
+
+    open override var quaternion: CMQuaternion {
+        get {
+            return _quaternion
+        }
+        set {
+            _quaternion = newValue
+        }
+    }
+
+    open override func multiply(byInverseOf attitude: CMAttitude) {
+        multiplyAttitude = attitude
+        multiplyCalled = true
+    }
+
+    // Instance Interface
+    open var multiplyCalled = false
+    open var multiplyAttitude: CMAttitude?
+
+}
---
+++ MockCoreMotionTests/MockCMAttitudeTests.swift
@@ -0,0 +1,63 @@
+//
+//  MockCMAttitudeTests.swift
+//  MockCoreMotion
+//
+//  Created by Chunyu Ou on 3/12/17.
+//  Copyright © 2017 Chunyu Ou. All rights reserved.
+//
+
+import XCTest
+import CoreMotion
+@testable import MockCoreMotion
+
+class MockCMAttitudeTests: XCTestCase {
+    
+    var testMockCMAttitude: MockCMAttitude?
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        testMockCMAttitude = MockCMAttitude()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        testMockCMAttitude = nil
+        super.tearDown()
+    }
+    
+    func testAllInstanceVariables() {
+        XCTAssertEqual(testMockCMAttitude!.roll, 0)
+        XCTAssertEqual(testMockCMAttitude!.yaw, 0)
+        XCTAssertEqual(testMockCMAttitude!.pitch, 0)
+        XCTAssertNotNil(testMockCMAttitude!.rotationMatrix)
+        XCTAssertNotNil(testMockCMAttitude!.quaternion)
+        let testRoll = 5.0
+        let testYaw = 10.0
+        let testPitch = 4.0
+        var testRotationMatrix = CMRotationMatrix()
+        testRotationMatrix.m11 = 12345.12345
+        var testQuaternion = CMQuaternion()
+        testQuaternion.w = 123.123
+        testMockCMAttitude?.roll = testRoll
+        testMockCMAttitude?.yaw = testYaw
+        testMockCMAttitude?.pitch = testPitch
+        testMockCMAttitude?.rotationMatrix = testRotationMatrix
+        testMockCMAttitude?.quaternion = testQuaternion
+        XCTAssertEqual(testMockCMAttitude!.roll, testRoll)
+        XCTAssertEqual(testMockCMAttitude!.yaw, testYaw)
+        XCTAssertEqual(testMockCMAttitude!.pitch, testPitch)
+        XCTAssertEqual(testMockCMAttitude!.rotationMatrix.m11, testRotationMatrix.m11)
+        XCTAssertEqual(testMockCMAttitude!.quaternion.w, testQuaternion.w)
+    }
+    
+    func testMultiply() {
+        XCTAssertFalse(testMockCMAttitude!.multiplyCalled, "multiply() should never be called.")
+        XCTAssertNil(testMockCMAttitude!.multiplyAttitude)
+        let testAttitude = MockCMAttitude()
+        testMockCMAttitude?.multiply(byInverseOf: testAttitude)
+        XCTAssertTrue(testMockCMAttitude!.multiplyCalled, "multiply() should be called.")
+        XCTAssertEqual(testMockCMAttitude!.multiplyAttitude, testAttitude)
+    }
+    
+}

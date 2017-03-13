58b06622cfb75cea75af95689e676f47f6b718e1
Chunyu Ou <chunyu@unify.id>
2017-03-12 20:59:54 -0700

    add unit tests for MockCMAltimeter

--- MockCoreMotionTests/MockCMAltimeterTests.swift
+++ MockCoreMotionTests/MockCMAltimeterTests.swift
@@ -7,11 +7,21 @@
 //
 
 import XCTest
+import CoreMotion
 @testable import MockCoreMotion
 
 class MockCMAltimeterTests: XCTestCase {
     
     var testMockCMAltimeter: MockCMAltimeter?
+    class myTestError: Error {
+        var _description = ""
+        init(description: String) {
+            _description = description
+        }
+        func description() -> String {
+            return _description
+        }
+    }
     
     override func setUp() {
         super.setUp()
@@ -25,4 +35,78 @@ class MockCMAltimeterTests: XCTestCase {
         super.tearDown()
     }
     
+    func testIsRelativeAltitudeAvailable() {
+        XCTAssertFalse(MockCMAltimeter.isRelativeAltitudeAvailable(), "isRelativeAltitudeAvailable() should return false by default.")
+        MockCMAltimeter._isRelativeAltitudeAvailable = true
+        XCTAssertTrue(MockCMAltimeter.isRelativeAltitudeAvailable())
+    }
+    
+    func testStartRelativeAltitudeUpdates() {
+        let defaultExpectation = expectation(description: "startRelativeAltitudeUpdates() should invoke handler callback with both params nil by default.")
+        var testData: CMAltitudeData?
+        var testError: Error?
+        XCTAssertFalse(testMockCMAltimeter!.startRelativeAltitudeUpdatesCalled, "startRelativeAltitudeUpdates() should never be called.")
+        testMockCMAltimeter?.startRelativeAltitudeUpdates(to: OperationQueue.main) {
+            (data, error) in
+            testData = data
+            testError = error
+            defaultExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 3) {
+            [unowned self](error) in
+            guard error == nil else {
+                XCTFail("Error found in expectation")
+                return
+            }
+            XCTAssertNil(testData)
+            XCTAssertNil(testError)
+            XCTAssertTrue(self.testMockCMAltimeter!.startRelativeAltitudeUpdatesCalled, "startRelativeAltitudeUpdates() should be called.")
+        }
+        
+        let dataExpectation = expectation(description: "startRelativeAltitudeUpdates() should invoke handler callback with custome data as params.")
+        let testCMAltitudeData = CMAltitudeData()
+        testMockCMAltimeter?.startRelativeAltitudeUpdatesHandlerData = testCMAltitudeData
+        testMockCMAltimeter?.startRelativeAltitudeUpdates(to: OperationQueue.main) {
+            (data, error) in
+            testData = data
+            testError = error
+            dataExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 3) {
+            (error) in
+            guard error == nil else {
+                XCTFail("Error found in expectation")
+                return
+            }
+            XCTAssertEqual(testData, testCMAltitudeData)
+            XCTAssertNil(testError)
+        }
+        testMockCMAltimeter?.startRelativeAltitudeUpdatesHandlerData = nil
+        let testErrorDescription = "Test Error"
+        testMockCMAltimeter?.startRelativeAltitudeUpdatesHandlerError = myTestError(description: testErrorDescription)
+        let errorExpectation = expectation(description: "startRelativeAltitudeUpdates() should invoke handler callback with custome error as params.")
+        testMockCMAltimeter?.startRelativeAltitudeUpdates(to: OperationQueue.main) {
+            (data, error) in
+            testData = data
+            testError = error
+            errorExpectation.fulfill()
+        }
+        waitForExpectations(timeout: 3) {
+            (error) in
+            guard error == nil else {
+                XCTFail("Error found in expectation")
+                return
+            }
+            let castedError = testError as! myTestError
+            XCTAssertNil(testData)
+            XCTAssertEqual(castedError.description(), testErrorDescription)
+        }
+    }
+    
+    func testStopRelativeAltitudeUpdates() {
+        XCTAssertFalse(testMockCMAltimeter!.stopRelativeAltitudeUpdatesCalled, "stopRelativeAltitudeUpdates() Should never be called")
+        testMockCMAltimeter?.stopRelativeAltitudeUpdates()
+        XCTAssertTrue(testMockCMAltimeter!.stopRelativeAltitudeUpdatesCalled, "stopRelativeAltitudeUpdates() Should be called")
+    }
+
 }

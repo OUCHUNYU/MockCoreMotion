3cd231a7e8b72022dcce190f8e3a34c09441da04
Chunyu Ou <ouchunyu@yahoo.com>
2017-06-10 12:18:46 -0700

    add some test, work in progress.

--- MockCoreMotionTests/MockCMMotionManagerTests.swift
+++ MockCoreMotionTests/MockCMMotionManagerTests.swift
@@ -7,29 +7,52 @@
 //
 
 import XCTest
+import CoreMotion
+@testable import MockCoreMotion
 
 class MockCMMotionManagerTests: XCTestCase {
+    
+    var testMockCMMotionManager: MockCMMotionManager?
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        testMockCMMotionManager = MockCMMotionManager()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+        testMockCMMotionManager = nil
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testAllInstanceVariablesAreMutable() {
+        XCTAssertFalse(testMockCMMotionManager!.isAccelerometerAvailable)
+        XCTAssertFalse(testMockCMMotionManager!.isAccelerometerActive)
+        XCTAssertFalse(testMockCMMotionManager!.isGyroAvailable)
+        XCTAssertFalse(testMockCMMotionManager!.isGyroActive)
+        XCTAssertFalse(testMockCMMotionManager!.isMagnetometerAvailable)
+        XCTAssertFalse(testMockCMMotionManager!.isMagnetometerActive)
+        XCTAssertFalse(testMockCMMotionManager!.isDeviceMotionAvailable)
+        XCTAssertFalse(testMockCMMotionManager!.isDeviceMotionActive)
+        
+        testMockCMMotionManager!.isAccelerometerAvailable = true
+        testMockCMMotionManager!.isAccelerometerActive = true
+        testMockCMMotionManager!.isGyroAvailable = true
+        testMockCMMotionManager!.isGyroActive = true
+        testMockCMMotionManager!.isMagnetometerAvailable = true
+        testMockCMMotionManager!.isMagnetometerActive = true
+        testMockCMMotionManager!.isDeviceMotionAvailable = true
+        testMockCMMotionManager!.isDeviceMotionActive = true
+        
+        XCTAssertTrue(testMockCMMotionManager!.isAccelerometerAvailable)
+        XCTAssertTrue(testMockCMMotionManager!.isAccelerometerActive)
+        XCTAssertTrue(testMockCMMotionManager!.isGyroAvailable)
+        XCTAssertTrue(testMockCMMotionManager!.isGyroActive)
+        XCTAssertTrue(testMockCMMotionManager!.isMagnetometerAvailable)
+        XCTAssertTrue(testMockCMMotionManager!.isMagnetometerActive)
+        XCTAssertTrue(testMockCMMotionManager!.isDeviceMotionAvailable)
+        XCTAssertTrue(testMockCMMotionManager!.isDeviceMotionActive)
     }
     
 }
